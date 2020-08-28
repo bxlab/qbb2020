2a6d987af9fc1c204f0a78db953b9f8c91103644
msauria <crockopotamus@gmail.com>
2020-08-28 16:51:35 -0400

    Added day3/3-afternoon

---
+++ day3/3-afternoon/prep.sh
@@ -0,0 +1,3 @@
+# Create a fasta file from a set of transcripts
+
+gffread /Users/cmdb/data/results/stringtie/SRR072893/SRR072893.gtf -r 2L:1-1000000 -g /Users/cmdb/data/results/BDGP6.fa -w subset.fa
